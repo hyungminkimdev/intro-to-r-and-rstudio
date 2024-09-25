44613559d0addad8b3854bc93186aca5c959b56c
Henrykim1111 <henrykim9319@gmail.com>
2024-09-24 20:09:03 -0400

    class finished

--- intro-r/script.R
+++ intro-r/script.R
@@ -61,5 +61,76 @@ members[members > 6]
 members[members == 6] # Find where is '6'
 length(members[members == 6]) # Find counts of '6'
 sum(members == 6)
+sum(members == 2)
 
+##### Missing Data #####
 
+# Missing data is NA
+
+members_na <- c(members, 3, NA)
+sum(members_na)
+sum(members_na)
+members_na
+
+# Remove missing values within functions na.rm = TRUE
+sum(members_na, na.rm = TRUE)
+mean(members_na, na.rm = TRUE)
+
+##### Importing data #####
+library(tidyverse)
+
+read_csv("data/SAFI_clean.csv")
+
+interviews <- read_csv("data/SAFI_clean.csv")
+# $ view(interviews)
+
+class(interviews)
+
+interviews <- interviews[-1, ] # remove fisrt data
+interviews
+
+nrow(interviews) # number of rows
+ncol(interviews) # number of columns
+dim(interviews) # dimension
+
+names(interviews)
+glimpse(interviews)
+        
+summary(interviews)
+
+##### Subset data frames #####
+interviews[2]
+interviews["village"]
+village_obj <- "village"
+interviews[village_obj]
+
+# Two dimensions
+interviews[1, 6]
+interviews[, 6]
+interviews[6, ]
+
+interviews_sub <- interviews[1:6, c(1, 3, 4)]
+summary(interviews_sub)
+
+##### Extract vectors #####
+interviews[["no_membrs"]]
+interviews$memb_assoc
+
+##### Dates #####
+dates <- interviews$interview_date
+dates
+
+class(dates)
+as_date(dates)
+
+dates_clean <- as_date(dates)
+class(dates_clean)
+
+interviews$interview_date <- dates_clean # remove time and clean it to date
+day(dates) # can extract and see only day
+month(dates)
+year(dates)
+
+interviews$day <- day(dates)
+interviews$month <- month(dates)
+glimpse(interviews)
