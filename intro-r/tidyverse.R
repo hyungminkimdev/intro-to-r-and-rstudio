3c60d33f84583530ed6bf4bba702a21943fed307
Henrykim1111 <henrykim9319@gmail.com>
2024-10-30 09:29:05 -0400

    Class begin 241030

---
+++ intro-r/tidyverse.R
@@ -0,0 +1,9 @@
+dir.create("fig_output")
+
+# download.file(
+# "https://raw.githubusercontent.com/datacarpentry/r-socialsci/main/episodes/data/SAFI_clean.csv",
+# "data/SAFI_clean.csv", mode = "wb"
+# )
+
+# install.packages("tidyverse")
+library(tidyverse)
